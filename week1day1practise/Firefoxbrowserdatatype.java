4d77c8003d2572a3f128e215f19f2a405b66c3e6
seshaa26 <seshaa26@gmail.com>
2025-01-19 08:37:38 +0530

    Add files via upload
    Week1day1 assignments completed

---
+++ week1day1practise/Firefoxbrowserdatatype.java
@@ -0,0 +1,20 @@
+package week1day1practise;
+
+public class Firefoxbrowserdatatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 1.115f ;
+		
+		String browserName = "Firefox";
+		boolean isVisible = true;
+		char logo = 'F' ;
+		int yearReleased = 1998; 
+		
+	System.out.println("Browser Name : "+browserName + 
+			"\nBrowser version : "+browserVersion +  
+			"\nReleased in Year :"+ yearReleased + 
+			"\nBrowser LOGO :"+logo  );	
+	}
+
+}
---
+++ week1day1practise/PrintHello.java
@@ -0,0 +1,12 @@
+package week1day1practise;
+
+public class PrintHello {
+
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("Print Hello");
+	}
+
+}
